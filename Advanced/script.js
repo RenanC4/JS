d03db99b20cd49519d7cae47a68f8b51d420889f
renanc4 <renanc433@gmail.com>
2018-05-05 23:08:54 -0300

    Closures, Bind Call and Apply

--- Advanced/script.js
+++ Advanced/script.js
@@ -136,9 +136,106 @@ interview('Web developer')('Blanka'); //Usando dessa forma nao eh necessario cri
 //     console.log(score>=5);
 // }
 // game();
+/*
 hue = 5;
 (function(luckDay) {
     var score = Math.random() *10;
     console.log(score>=(5  - luckDay));
     console.log(score);
 })(hue);
+
+//Closures
+
+function aposentadoria(idadeParaAposentar){
+    var a = ' Anos para se aposentar.';
+    return function(anoDeNascimento){
+       var idade = 2018 - anoDeNascimento;
+        console.log((idadeParaAposentar - idade) + a);
+    }
+}
+
+aposentadoria(65)(1996);
+
+// funcao perguntas usando closures
+
+function interview(job){
+    return function(name){
+        if (job === 'Web Developer'){
+            console.log(name + ' Voce Manja dos Paranaue de WEBDEV?');
+        } else if (job === 'Designer'){
+            console.log(name + ' Voce manja dos paranaue de UX?');
+        } else {console.log(name + ' Me conta mais do seu trampo');
+        
+        }
+    }
+
+}
+
+interview('Web Developer')('Renan');
+interview('Imigrante Ilegal')('Alberto');  
+interview('Designer')('Joseph'); 
+*/   
+
+//Bind, call and apply
+/*
+var renan = {
+    name: 'Renan',
+    age: 22,
+    job: 'Web Developer',
+    presentation: function(style, timeOfDay){
+    if(style === 'Formal'){
+        console.log('Good ' + timeOfDay + ' Ladies and Gentleman I\'m ' + this.name + ' I\'m a ' + this.job + ' And I\'m ' + this.age + ' Years Old');
+    }else if (style === 'Informal'){
+        console.log('Coé Rapazeada, good ' + timeOfDay + ' Ladies and Gentleman I\'m ' + this.name + ' I\'m a ' + this.job + ' And I\'m ' + this.age + ' Years Old');
+    }
+
+}
+}
+
+var carol = {
+    name: 'Carol',
+    age: 25,
+    job: 'designer'
+};
+
+renan.presentation('Informal', 'Afternoon');
+
+renan.presentation.call(carol , 'Formal', 'Night');
+
+renan.presentation.apply(carol, ['Informal', 'Morning']);
+
+var renanFormal = renan.presentation.bind(renan, 'Formal');
+
+renanFormal('Night');
+renanFormal('Morning');
+
+var carolInformal = renan.presentation.bind(carol, 'Informal');
+carolInformal('Afternoon');
+*/
+
+var years = [1990, 1995, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn){
+    var arrRes = [];
+    for(var i = 0; i < arr.length; i++){
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el){
+    return 2018 - el;
+}
+
+function yearsToRetirement(el){
+    return 65 - (2018 - el);
+}
+
+function isFullAge(variableAge , el){
+    return el > variableAge;
+}
+
+var ages = arrayCalc(years, calculateAge);
+var isFullAgeBrasil = arrayCalc(ages, isFullAge.bind(this, 18));
+console.log(ages);
+console.log(isFullAgeBrasil);
